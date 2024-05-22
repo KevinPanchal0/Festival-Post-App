3c47736a271b88932f6ff3a978b90f8647772bf1
Kevin Panchal <151813310+KevinPanchal0@users.noreply.github.com>
2024-05-22 10:03:57 +0530

    Festival App

--- festival_post_app/lib/screen/history_screen.dart
+++ festival_post_app/lib/screen/history_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -25,43 +26,82 @@ class _HistoryScreenState extends State<HistoryScreen> {
     });
   }
 
+  Future<void> deleteImage(File file) async {
+    try {
+      await file.delete();
+      loadImage(); // Reload the images after deletion
+    } catch (e) {
+      print("Error deleting file: $e");
+    }
+  }
+
+  Future<void> shareImage(File file) async{
+    try {
+      XFile xFile = XFile(file.path);
+      await Share.shareXFiles([xFile]);
+    } catch (e){
+      print("Error sharing file: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: loadImage, icon: Icon(Icons.restore_rounded))
+          IconButton(onPressed: loadImage, icon: const Icon(Icons.restore_rounded))
         ],
       ),
-      body: ListView.builder(
-          itemCount: editedImage.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: ListTile(
-                title: Text(editedImage[index].path.split('/').last),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(
+          editedImage.length,
+          (index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
                 onTap: () {
                   showDialog(
-
-                      context: context,
-                      barrierDismissible: true,
-                      builder: (context) {
-                        return SimpleDialog(
-
-                          children: [
-                            Image(
-                              width: 250,
-                                height: 250,
-                                image:
-                                Image.file(File(editedImage[index].path),).image),
-                          ]
-                        );
-                      });
-
-                  // Navigator.push(context, MaterialPageRoute(builder: (context) => HistoryPreview(image: editedImage[index].path)));
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text(
+                          'Options',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              deleteImage(editedImage[index]);
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Delete'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              shareImage(editedImage[index]);
+                            },
+                            child: const Text('Share'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 },
+                child: Card(
+                  elevation: 5,
+                  child: Image(
+                      width: 250,
+                      height: 250,
+                      image: Image.file(
+                        File(editedImage[index].path),
+                      ).image),
+                ),
               ),
             );
-          }),
+          },
+        ),
+      ),
     );
   }
 
@@ -76,5 +116,4 @@ class _HistoryScreenState extends State<HistoryScreen> {
     }
     return editedImage;
   }
-
 }
